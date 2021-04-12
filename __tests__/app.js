d32ca0745cbaa6c0ffc1f9d7dd8463b3dc3d618d
Sharp Ninja <ninja@thesharp.ninja>
2021-04-12 10:17:35 -0500

    Basics configured

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,17 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
 
-describe('generator-service-definition-model:app', () => {
+describe("generator-service-definition-model:app", () => {
   beforeAll(() => {
+    console.log(__dirname);
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts(/* { someAnswer: true } */);
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("creates files", () => {
+    assert.file(["service-package.tmpl.js"]);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -2,24 +2,45 @@
 const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
+const fs = require("fs");
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts);
+
+    this.initialize = function() {
+      this.destinationRoot("./dist");
+      var dataPath = this.destinationPath("data.json");
+
+      if (fs.existsSync(dataPath)) {
+        const json = fs.readFileSync(dataPath);
+        this.props = JSON.parse(json);
+      }
+    };
+
+    this.initialize();
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(
-        `Welcome to the pioneering ${chalk.red(
-          "generator-service-definition-model"
-        )} generator!`
-      )
+      yosay(`Welcome to ${chalk.red("Service Definition Model")} generator!`)
     );
 
     const prompts = [
       {
-        type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
+        type: "input",
+        name: "serviceContextName",
+        message: "What is the name of your service context?",
+        default: this.props.serviceContextName || "default"
+      },
+      {
+        type: "list",
+        choices: ["gRPC", "REST", "WCF", "Custom"],
+        name: "serviceContextFramework",
+        message: "What is the Framework for your Service?",
+        default: this.props.serviceContextFramework || "gRPC"
       }
     ];
 
@@ -30,10 +51,17 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
-    );
+    var templatePath = this.templatePath("service-package.handlebars");
+    const template = fs.readFileSync(templatePath).toLocaleString();
+    const Handlebars = require("handlebars");
+    const json = JSON.stringify(this.props);
+    var compiled = Handlebars.compile(template, {
+      data: json
+    });
+    const result = compiled(this.props);
+    console.log(`compiled: ${result}`);
+    fs.writeFileSync(this.destinationPath("service-package.puml"), result);
+    fs.writeFileSync(this.destinationPath("data.json"), json);
   }
 
   install() {
